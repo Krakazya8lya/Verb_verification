1b9e40023996fe9de8474e5e0f4d02431a53048a
Krakazya8lya <kulshin2007@outlook.com>
2019-05-06 20:54:04 +0700

    add read file in array

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,30 @@
 
 using namespace std;
 
-bool getWord(ifstream &ifstream, string &word) {
-    word = "";
+struct EngVerbs {
+    string rusVerb;
+    string engVerb[4];
+} sVerbs[100];
+
+bool getStructVerb(ifstream &ifstream, EngVerbs &sVer) {
+    EngVerbs *sVerb = new EngVerbs;
     string line;
 
     if(!getline(ifstream, line)) return nullptr;
     int spaceVelue = 0;
+    string word;
     for(char letter : line) {
-        if(spaceVelue >= 3) word += letter;
-        if (letter == ' ') spaceVelue++;
+        if (spaceVelue >= 3) word += letter;
+        if (letter == ' ' && spaceVelue<3) {
+            sVerb->engVerb[spaceVelue] = word;
+            word = "";
+            spaceVelue++;
+        }
+        if(spaceVelue<3)
+        word+=letter;
     }
-
+    sVerb->rusVerb = word;
+    sVer = *sVerb;
     return true;
 }
 
@@ -22,11 +35,17 @@ bool getWord(ifstream &ifstream, string &word) {
 int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "Russian");
 
+
     ifstream file("../res/verbs.txt");
 
-    string word;
-    while (getWord(file, word)) {
-        cout << word << endl;
+    EngVerbs sVerb; int i= 0;
+    while (getStructVerb(file, sVerb)) {
+        sVerbs[i].rusVerb = sVerb.rusVerb;
+        sVerbs[i].engVerb[0] = sVerb.engVerb[0];
+        sVerbs[i].engVerb[1] = sVerb.engVerb[1];
+        sVerbs[i].engVerb[2] = sVerb.engVerb[2];
+        sVerbs[i].engVerb[3] = sVerb.engVerb[3];
+        i++;
     }
     return 0;
 }
